d2c0bab3a996507769e41b3f157c186aedf614e5
Riku Särkinen <riku@adbar.io>
2024-09-18 00:29:28 +0100

    chore: add cssnano to minify css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,21 @@
+const cssnano = {
+  preset: [
+    "default",
+    {
+      discardComments: { removeAll: true },
+      mergeRules: false,
+      reduceInitial: false,
+      zindex: false,
+    },
+  ],
+};
+
 const config = {
   plugins: {
     "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano } : {}),
   },
 };
 
